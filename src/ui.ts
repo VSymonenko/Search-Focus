825fc48e9a3208540e2030524041f9ed42b50722
VSymonenko <simonenko744@gmail.com>
2020-04-06 12:14:30 +0300

    back to simple version

--- src/ui.ts
+++ src/ui.ts
@@ -1,57 +1,35 @@
-import Vue, { CreateElement, VNode } from 'vue';
-import { Component } from 'vue-property-decorator';
-import Vuetify, { VTextField } from 'vuetify/lib';
+const ff = document.getElementById('find-and-focus');
+const input = document.createElement('input');
+const ul = document.createElement('ul');
 
-Vue.use(Vuetify)
+input.type = 'search';
+input.autofocus = true;
+input.placeholder = 'type for searching';
 
-@Component({
-  name: 'Input',
-})
-class Input extends Vue {
-  render(createElement: CreateElement): VNode {
-    return createElement(VTextField);
+const find = (items: string[], value: string): string[] => {
+  return items
+    .filter((it) => it.includes(value))
+    .sort();
+}
+
+const updateList = (event: Event) => {
+  if (event.target instanceof HTMLInputElement) {
+    const { value } = event.target;
+    if (value.length < 1) { return; }
+    const result = find(['asd', '21', 'asda1', 'asd21sda'], '21'); // TODO: get array value from figma API
+  
+    result.forEach((item: string) => {
+      const li = document.createElement('li');
+      li.textContent = item;
+      ul.appendChild(li);
+    });
   }
+  
 };
 
+input.addEventListener('input', updateList);
 
-new Vue({
-  el: '#find-and-focus',
-  render: (h: CreateElement) => h('div', {}, [
-    h(Input),
-  ]),
-});
 
-// const ff = document.getElementById('find-and-focus');
-// const ul = document.createElement('ul');
-// 
-// input.type = 'search';
-// input.autofocus = true;
-// input.placeholder = 'type for searching';
-// 
-// const find = (items: string[], value: string): string[] => {
-  // return items
-    // .filter((it) => it.includes(value))
-    // .sort();
-// }
-// 
-// const updateList = (event: InputEvent) => {
-  // if (event.target instanceof HTMLInputElement) {
-    // const { value } = event.target;
-    // if (value.length < 1) { return; }
-    // const result = find(['asd', '21', 'asda1', 'asd21sda'], '21'); // TODO: get array value from figma API
-  // 
-    // result.forEach((item: string) => {
-      // const li = document.createElement('li');
-      // li.textContent = item;
-      // ul.appendChild(li);
-    // });
-  // }
-  // 
-// };
-// 
-// input.addEventListener('input', updateList);
-// 
-
-// [input, ul].forEach(item => {
-//   ff.append(item);
-// })
+[input, ul].forEach(item => {
+  ff?.append(item);
+})
