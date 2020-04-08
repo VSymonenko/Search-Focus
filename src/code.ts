7453e80b6b8ebcaeac93eb32a973775180d3c251
VSymonenko <simonenko744@gmail.com>
2020-04-08 12:22:18 +0300

    create map

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,23 @@
-// ❌ 1. get all frame names 
-// ❌ 2. build Map
-import figma from './index';
+// ✅ 1. get all frame names 
+// ✅ 2. build Map
+
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
+
+interface FrameKey {
+  name: string;
+  id: string;
+}
+
+const map = new WeakMap<FrameKey, SceneNode>();
+
+const createMapRecursive = (root: readonly SceneNode[], map: WeakMap<FrameKey, SceneNode>): void => {
+  root.forEach((node: SceneNode) => {
+    map.set({name: node.name, id: node.id}, node);
+    if ('children' in node) {
+      createMapRecursive(node.children, map);
+    }
+  });
+};
+
+createMapRecursive(figma.currentPage.children, map);
