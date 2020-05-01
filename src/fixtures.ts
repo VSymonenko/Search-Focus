93bb5288d6233170eaab68a7b4f175d109f54756
VSymonenko <simonenko744@gmail.com>
2020-05-01 17:23:28 +0300

    feat: add fixtures

---
+++ src/fixtures.ts
@@ -0,0 +1,22 @@
+export const fList: FrameKey[] = [
+  {
+    name: 'asdf',
+    id: '',
+  },
+  {
+    name: 'asd',
+    id: '',
+  },
+  {
+    name: '2asd',
+    id: '',
+  },
+  {
+    name: 'aSd',
+    id: '',
+  },
+  {
+    name: 'asd111',
+    id: '',
+  },
+];
