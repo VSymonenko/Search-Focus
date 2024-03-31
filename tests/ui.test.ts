34c7b2e34a6d1941e572089f24f13103ea4d2295
V.Symonenko <simonenko744@gmail.com>
2024-03-31 14:54:51 +0300

    chore(test): refactoring ui import

--- tests/ui.test.ts
+++ tests/ui.test.ts
@@ -1,11 +1,5 @@
-import {
-  FindOptions,
-  appendList,
-  clearNode,
-  debounce,
-  find,
-  focusOnFrame,
-} from '../src/ui';
+import * as ui from '../src/ui';
+
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 
 test('should be clear children nodes', () => {
@@ -13,7 +7,7 @@ test('should be clear children nodes', () => {
   const child = document.createElement('div');
   root.appendChild(child);
   
-  clearNode(root);
+  ui.clearNode(root);
   expect(root.children.length).toEqual(0);
 });
 
@@ -21,34 +15,34 @@ describe('find', () => {
   const list = [{ name: 'foo' }, { name: 'bar' }] as FrameKey[];
 
   test('should be case sensitive', () => {
-    const result = find(list, 'Foo', { caseSensitive: true } as FindOptions);
+    const result = ui.find(list, 'Foo', { caseSensitive: true } as FindOptions);
     expect(result.length).toEqual(0);
 
-    const result2 = find(list, 'foo', { caseSensitive: true } as FindOptions);
+    const result2 = ui.find(list, 'foo', { caseSensitive: true } as FindOptions);
     expect(result2.length).toEqual(1);
   });
 
   test('should be case insensitive', () => {
-    const result = find(list, 'Foo');
+    const result = ui.find(list, 'Foo');
     expect(result.length).toEqual(1);
   });
 
   const list2 = [...list, { name: 'foobar' } as FrameKey];
   test('should be math whole word', () => {
-    const result = find(list2, 'oo', { bounderies: true } as FindOptions);
+    const result = ui.find(list2, 'oo', { bounderies: true } as FindOptions);
     expect(result.length).toEqual(0);
 
-    const result2 = find(list2, 'foo', { bounderies: true } as FindOptions);
+    const result2 = ui.find(list2, 'foo', { bounderies: true } as FindOptions);
     expect(result2.length).toEqual(1);
 
-    const result3 = find(list2, 'oo');
+    const result3 = ui.find(list2, 'oo');
     expect(result3.length).toEqual(2);
   });
 });
 
 test('should be focusing on Node', () => {
   const spy = vi.spyOn(globalThis.parent, 'postMessage');
-  focusOnFrame({ name: 'foo' } as FrameKey);
+  ui.focusOnFrame({ name: 'foo' } as FrameKey);
   expect(spy).toHaveBeenCalledTimes(1);
 });
 
@@ -63,7 +57,7 @@ describe('debounce', () => {
   test('should be invoking callback after delay', () => {
     const DELAY = 100;
     const mock = vi.fn();
-    const debounced = debounce(mock, DELAY);
+    const debounced = ui.debounce(mock, DELAY);
     debounced();
     expect(mock).not.toHaveBeenCalled();
     vi.advanceTimersByTime(DELAY + 1);
@@ -75,6 +69,6 @@ test('should be appending list', () => {
   const ul = document.createElement('ul');
   const list = [{ name: 'foo' }, { name: 'bar' }] as FrameKey[];
   const spy = vi.spyOn(ul, 'appendChild');
-  appendList(list, ul);
+  ui.appendList(list, ul);
   expect(spy).toHaveBeenCalledTimes(2);
 });
